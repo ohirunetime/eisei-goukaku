2de627d4dcb6a38797ef6f03e479d48182b9d0da
ohirune <ohirune.timer@gmail.com>
2025-07-03 07:12:51 +0900

    ログアウト toast １回のみ発火するようuseRef使用

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -1,13 +1,17 @@
 import React from "react";
-import { useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { navigate } from "gatsby";
 import { toast } from "react-hot-toast";
 
-export default function LoginPage() {
+export default function LogoutPage() {
     const { logout } = useAuth();
+    const hasLoggedOut = useRef(false);
 
     useEffect(() => {
+        if (hasLoggedOut.current) return;
+        hasLoggedOut.current = true;
+
         logout().then(() => {
             toast.success("ログアウトしました");
             navigate("/login")
